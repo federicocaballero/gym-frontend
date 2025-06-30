c61b84ff735c71e9046eb03543636e1d8e3c7be8
Federico Caballero <fedecaballero1234@gmail.com>
2025-06-30 11:46:11 -0300

    Subiendo todos los últimos cambios a main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
-import Dashboard from './pages/Dashboard'
+import './App.css'
+import { SearchMember } from "./features/member-search/SearchMember";
+import { CollapsibleSection } from "./components/shared/Colapsible";
+import SearchIcon from '@mui/icons-material/Search';
+import { CreateMemberForm } from './features/create-member/CreateMember';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { UpdatePaymentForm } from './features/payment-update/PaymentUpdateCard';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Dashboard/>
-  )
+    <div style={{ padding: "2rem" }}>
+      <CollapsibleSection
+        title="Buscar Alumno"
+        icon={<SearchIcon />}
+        defaultExpanded={true}
+      >
+        <SearchMember />
+      </CollapsibleSection>
+
+      <CollapsibleSection
+        title="Create Member"
+        icon={<AddCircleIcon />}
+        defaultExpanded={true}
+      >
+        <CreateMemberForm onSubmit={() => {
+          console.log('submit');
+        }} />
+      </CollapsibleSection>
+
+      <CollapsibleSection
+        title="Update Payment"
+        icon={<CurrencyExchangeIcon />}
+        defaultExpanded={true}
+      >
+        <UpdatePaymentForm />
+      </CollapsibleSection>
+    </div>
+  );
 }
 
 export default App
